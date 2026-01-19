310b8278496e643ae88f24415bf91073a7f40eef
Berrcir <berciforever123@gmail.com>
2026-01-19 21:33:59 +0700

    Feature: Подключен Swagger

--- DirectoryService/src/DirectoryService.Web/Program.cs
+++ DirectoryService/src/DirectoryService.Web/Program.cs
@@ -1,5 +1,16 @@
+﻿using DirectoryService.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddProgramDependencies();
+
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DirectoryService"));
+}
 
 app.Run();
