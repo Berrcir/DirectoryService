b2c5021d4ba3c44fc916e969a1f3de493a4e2c68
Berrcir <berciforever123@gmail.com>
2026-01-19 23:07:17 +0700

    Feature: Добавление сущностей модуля Positions

---
+++ DirectoryService/src/DirectoryService.Domain/Positions/Position.cs
@@ -0,0 +1,19 @@
+﻿using DirectoryService.Domain.Abstractions;
+
+namespace DirectoryService.Domain.Positions;
+
+public class Position: Entity
+{
+    public required PositionName Name { get; init; }
+
+    public string? Description { get; private set; }
+
+    public List<Guid> DepartmenPositiontIds { get; private set; } = [];
+
+    public Position(Guid id, PositionName name, string? description = default)
+        : base(id)
+    {
+        Name = name;
+        Description = description;
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Domain/Positions/PositionName.cs
@@ -0,0 +1,8 @@
+﻿namespace DirectoryService.Domain.Positions;
+
+public record class PositionName
+{
+    public required string Speciality { get; init; }
+
+    public required string Direction { get; init; }
+}
