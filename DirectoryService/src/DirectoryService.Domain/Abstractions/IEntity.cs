39b32737639e1af4e911b932a4f94d75ff6cbced
Berrcir <berciforever123@gmail.com>
2026-01-19 23:05:22 +0700

    Feature: Добавление абстрактной сущности

---
+++ DirectoryService/src/DirectoryService.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DirectoryService.Domain.Abstractions;
+
+public interface IEntity
+{
+    Guid Id { get; init; }
+
+    bool IsActive { get; init; }
+
+    DateTime CreatedAt { get; init; }
+
+    DateTime UpdatedAt { get; }
+}
